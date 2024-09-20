f64762ef203220209608756226f99527a00ed2d3
Angel Ivorra <angel.ivorra@gmail.com>
2024-09-20 22:44:51 +0200

    subida

--- bin/install-server.py
+++ bin/install-server.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     print('Instalamos servidor...')
     run(["sudo","apt","update"])
     run(["sudo","apt","upgrade","-y"])
-    run(["git","pull","origin/main"], cwd=r'/home/angel/lgptclient')
+    run(["git","pull","origin", "main"], cwd=r'/home/angel/lgptclient')
     #run(["rm","-r","/home/angel/lgptclient/venv"])
     #run(["python3", "-m", "venv", "/home/angel/lgptclient/venv"])
     
