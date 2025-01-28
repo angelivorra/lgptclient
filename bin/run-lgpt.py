80fb2c9e942f183e533b4e9a00f63a1344db1a38
Angel Ivorra <angel.ivorra@gmail.com>
2025-01-28 18:17:59 +0100

    Enhance audio server restart process in run-lgpt.py

--- bin/run-lgpt.py
+++ bin/run-lgpt.py
@@ -31,7 +31,12 @@ def restart_server():
   print('Restart Server...')
   run(["sudo", "killall", "arecord"])
   run(["sudo", "killall", "aplay"])
+  run(["sudo", "/etc/init.d/alsa-utils", "stop"])
+  run(["sudo", "/etc/init.d/alsa-utils", "start"])
   subprocess.Popen("sudo arecord -D hw:Loopback,1 -f cd | sudo aplay -D movida", shell=True)
+  # arecord_cmd = "sudo arecord -D hw:Loopback,1 -f cd -B 5000 -F 2500"
+  # aplay_cmd = "sudo aplay -D movida -B 5000 -F 2500"
+  # subprocess.Popen(f"{arecord_cmd} | {aplay_cmd}", shell=True)
   run(["sudo", "systemctl", "restart", "servidor"])
 
 if __name__ == "__main__":
