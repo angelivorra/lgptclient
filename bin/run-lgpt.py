e933151ffb6069fabe7e6ce0bb858c5745940fbd
Angel Ivorra <angel.ivorra@gmail.com>
2025-01-28 16:23:38 +0100

    Update audio configuration and enhance error handling in run-lgpt.py

--- bin/run-lgpt.py
+++ bin/run-lgpt.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env /home/angel/lgptclient/venv/bin/python3
 from subprocess import run
+import subprocess
 import sys
 
 LGPT = '/home/angel/lgptclient/lgpt/bin/lgpt.rpi-exe'
 DATABASE = "/home/angel/lgpt.data"
 
 def main():
-  while True:
-    restart_server()
-    try:
-      data = run(LGPT, capture_output=True, shell=True)
+    while True:
+        restart_server()
+        try:
+            data = run(LGPT, capture_output=True, shell=True)
 
-      output = data.stdout.splitlines()
-      errors = data.stderr.splitlines()
+            output = data.stdout.splitlines()
+            errors = data.stderr.splitlines()
 
-      print('output')
-      print(data.stdout)
-      print('Errores')
-      print(data.stderr)
+            print('output')
+            print(data.stdout)
+            print('Errores')
+            print(data.stderr)
 
-      print(data.returncode)
+            print(data.returncode)
 
-    except (EOFError, KeyboardInterrupt):
-      pass
+        except KeyboardInterrupt:
+            sys.exit()
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 def restart_server():
   print('Restart Server...')
+  run(["sudo", "killall", "arecord"])
+  run(["sudo", "killall", "aplay"])
+  subprocess.run("sudo arecord -D hw:Loopback,1 -f cd | sudo aplay -D movida", shell=True)
   run(["sudo", "systemctl", "restart", "servidor"])
 
 if __name__ == "__main__":
